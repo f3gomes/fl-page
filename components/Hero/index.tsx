1b62b823b1c3506a1b66b9ee32838a1aa0820cda
Felipe Gomes <fgomesdeluna@gmail.com>
2025-10-07 15:13:13 -0300

    feat: include client logos

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -11,46 +11,40 @@ const Hero = () => {
 
   return (
     <>
-      <section className="overflow-hidden pb-20 pt-35 md:pt-40 xl:pb-25 xl:pt-46">
-        <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
+      <section className="overflow-hidden pt-35 pb-20 md:pt-40 xl:pt-46 xl:pb-25">
+        <div className="max-w-c-1390 mx-auto px-4 md:px-8 2xl:px-0">
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
-            <div className=" md:w-1/2">
+            <div className="md:w-1/2">
               <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
-                🔥 Solid - A Complete SaaS Web Template
+                Soluções de TI seguras para um ambiente mais seguro
               </h4>
-              <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
-                Free Next.js Template for {"   "}
-                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
-                  SaaS
-                </span>
+              <h1 className="xl:text-hero mb-5 pr-16 text-3xl font-bold text-black dark:text-white">
+                Garantia, Qualidade e o Melhor Preço!
               </h1>
               <p>
-                Solid Pro - Packed with all the key integrations you need for
-                swift SaaS startup launch, including - Auth, Database, Sanity
-                Blog, Essential Components, Pages and More. Built-winth -
-                Next.js 13, React 18 and TypeScript.
+                "A qualidade nunca se obtém por acaso, ela é sempre o resultado
+                do esforço inteligente".
               </p>
 
               <div className="mt-10">
                 <form onSubmit={handleSubmit}>
                   <div className="flex flex-wrap gap-5">
-                    <input
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      type="text"
-                      placeholder="Enter your email address"
-                      className="rounded-full border border-stroke px-6 py-2.5 shadow-solid-2 focus:border-primary focus:outline-hidden dark:border-strokedark dark:bg-black dark:shadow-none dark:focus:border-primary"
-                    />
-                    <button
-                      aria-label="get started button"
-                      className="flex rounded-full bg-[#fe8d00] hover:bg-[#e67f00] px-7.5 py-2.5 text-white duration-300 ease-in-out dark:bg-btndark dark:hover:bg-blackho cursor-pointer"
+                    <a
+                      target="_blank"
+                      href="https://api.whatsapp.com/send?phone=5585999504382&text=Gostaria%2C%20de%20um%20or%C3%A7amento!"
                     >
-                      Get Started
-                    </button>
+                      <Image
+                        src="/images/icon/wpp.png"
+                        alt="logo"
+                        width={70}
+                        height={70}
+                        className="block transition-all duration-300 hover:scale-110 hover:opacity-90 hover:drop-shadow-lg"
+                      />
+                    </a>
                   </div>
                 </form>
 
-                <p className="mt-5 text-black dark:text-white">
+                <p className="invisible mt-5 text-black dark:text-white">
                   Try for free no credit card required.
                 </p>
               </div>
@@ -63,14 +57,14 @@ const Hero = () => {
                   alt="shape"
                   width={46}
                   height={246}
-                  className="absolute -left-11.5 top-0"
+                  className="absolute top-0 -left-11.5"
                 />
                 <Image
                   src="/images/shape/shape-02.svg"
                   alt="shape"
                   width={36.9}
                   height={36.7}
-                  className="absolute bottom-0 right-0 z-10"
+                  className="absolute right-0 bottom-0 z-10"
                 />
                 <Image
                   src="/images/shape/shape-03.svg"
@@ -79,16 +73,16 @@ const Hero = () => {
                   height={21.66}
                   className="absolute -right-6.5 bottom-0 z-1"
                 />
-                <div className=" relative aspect-700/444 w-full">
+                <div className="relative aspect-700/444 w-full">
                   <Image
-                    className="shadow-solid-l dark:hidden"
-                    src="/images/hero/hero-light.svg"
+                    className="shadow-solid-l mask-[radial-gradient(white_70%,transparent_100%)] dark:hidden"
+                    src="/images/hero/hero-light.avif"
                     alt="Hero"
                     fill
                   />
                   <Image
-                    className="hidden shadow-solid-l dark:block"
-                    src="/images/hero/hero-dark.svg"
+                    className="shadow-solid-l hidden dark:block"
+                    src="/images/hero/hero-dark.avif"
                     alt="Hero"
                     fill
                   />
